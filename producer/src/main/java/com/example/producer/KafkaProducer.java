0e399eb7f2d8083b32ad034321b3344994c54079
SutejJantali <iamsutej@gmail.com>
2026-07-27 01:29:14 +0530

    Initial commit

---
+++ producer/src/main/java/com/example/producer/KafkaProducer.java
@@ -0,0 +1,27 @@
+package com.example.producer;
+
+import org.apache.kafka.common.internals.Topic;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class KafkaProducer {
+	private final KafkaTemplate<String, RiderLocation> kafkaTemplate;
+	
+	public KafkaProducer(KafkaTemplate<String, RiderLocation> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+	}
+	
+	@PostMapping("/send")
+	public String sendMessage(@RequestParam String message) {
+		RiderLocation location = new RiderLocation("rider123", 28.61, 77.23);
+		kafkaTemplate.send("my-topic-new", location);
+		
+		return "Location sent: "+ message;
+	}
+}
